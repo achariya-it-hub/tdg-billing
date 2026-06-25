15f0d9a60d9b055b4a8848c14b103a1e31fdb9ce
Achariya-it <it.manager@achariya.org>
2026-06-25 22:59:49 +0530

    Switch API URL to Replit

--- ttt/lib/config.dart
+++ ttt/lib/config.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 class AppConfig {
-  static const String _productionUrl = 'https://darkslategrey-sandpiper-611004.hostingersite.com/api';
+  static const String _productionUrl = 'https://tdg-billing-itmanager16.repl.co/api';
 
   static String get baseUrl {
     const define = String.fromEnvironment('API_URL');
