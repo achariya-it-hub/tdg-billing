dfbfda94845d31e01425c3a6e47526177769aac2
Achariya-it <it.manager@achariya.org>
2026-09-18 10:56:06 +0530

    feat: set Vanilla Softy (39) and Choice of Dip (15) as tax inclusive items

--- server/index.js
+++ server/index.js
@@ -1625,19 +1625,21 @@ let menuItems = [
     "id": "m_vanilla_softy",
     "categoryId": "c4",
     "name": "Vanilla Softy",
-    "price": 39,
-    "description": "Creamy Vanilla Soft Serve Cone",
+    "price": 37.14,
+    "description": "Creamy Vanilla Soft Serve Cone (₹39 Inclusive of Tax)",
     "isAvailable": true,
-    "image": "/images/menu/vanilla softy.png"
+    "image": "/images/menu/vanilla softy.png",
+    "taxInclusive": true
   },
   {
     "id": "m_dip_choice",
     "categoryId": "c4",
     "name": "Choice of Dip",
-    "price": 15,
-    "description": "Choice of Dip (Garlic Mayo, Spicy Mayo, Honey Mustard, Tzatziki, Jalapeno Cheese, Turkish Chilli)",
+    "price": 14.29,
+    "description": "Choice of Dip (Garlic Mayo, Spicy Mayo, Honey Mustard, Tzatziki, Jalapeno Cheese, Turkish Chilli) (₹15 Inclusive of Tax)",
     "isAvailable": true,
-    "image": "/images/menu/garlic mayo.png"
+    "image": "/images/menu/garlic mayo.png",
+    "taxInclusive": true
   },
   {
     "id": "m_kombucha_mint",
